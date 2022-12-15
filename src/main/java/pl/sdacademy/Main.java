6e637edd40bd3736929aeff9d8150c4ddf6216aa
BartN77 <116747040+BartN77@users.noreply.github.com>
2022-12-15 20:55:17 +0100

    20:54

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -8,5 +8,7 @@ public class Main {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Hello world BITCHES!!!!!");
+    System.out.println("20:54");
+
   }
 }
