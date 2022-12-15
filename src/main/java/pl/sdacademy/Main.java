a2b1484b37b24d47c8923e44ae461eaffb4c81b9
eMThe94 <marek.t1994@interia.pl>
2022-12-15 21:19:58 +0100

    Test02

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -11,5 +11,6 @@ public class Main {
     System.out.println("Hello world!");
     System.out.println("Hello world!");
     System.out.println("Test MT");
+    System.out.println("chujnia in test");
   }
 }
