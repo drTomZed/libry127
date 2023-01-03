df1cec81368cd546b9e2dec253eb5b899a971f39
drTomZed <tomasz.zajac403@gmail.com>
2023-01-03 20:43:03 +0100

    dodany test połaczenia

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,11 +1,29 @@
 package pl.sdacademy;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.EntityManager;
+
+import static pl.sdacademy.HibernateUtil.getSessionFactory;
+
 /**
  * @author mszyrner
  */
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello jędrna dupa!");
+    public static void main(String[] args) {
+        System.out.println("Hello jędrna dupa!");
+        Session session = getSessionFactory().openSession();
 
-  }
+
+        //Check databse vaersion
+        String sql = "Select version()";
+
+        String result = (String) session.createNativeQuery(sql).getSingleResult();
+        System.out.println(result);
+
+        session.close();
+        HibernateUtil.shutdown();
+
+    }
 }
