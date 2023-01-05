3c6f1296ce418fb2fc285123b92c1eccf237493f
Uzytkownik <tomasz.zajac403@gmail.com>
2023-01-05 16:13:42 +0100

    1

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,41 +1,42 @@
 package pl.sdacademy;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
+import pl.sdacademy.dao.BookRepositoryImpl;
 
-import javax.persistence.EntityManager;
-
-import static pl.sdacademy.HibernateUtil.getSessionFactory;
-
-
-//public class Main {
-//    public static void main(String[] args) {
-//
-//        Session session = getSessionFactory().openSession();
-//
-//
-//        //Check databse vaersion
-//        String sql = "Select version()";
-//
-//        String result = (String) session.createNativeQuery(sql).getSingleResult();
-//        System.out.println(result);
-//
-//        session.close();
-//        HibernateUtil.shutdown();
-//
-//    }
-
-
-
-
-import pl.sdacademy.ui.ConsoleInterface;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-    ConsoleInterface consoleInterface= new ConsoleInterface();
-    consoleInterface.chooseOption();
-
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("1. Usun ksiazke");
+            System.out.println("2. Dodaj ksiazke");
+            System.out.println("3. Znajdz ksiazke po id");
+            System.out.println("4. Wypisz wszyskie ksiazki");
+            System.out.println("0. Wyjdz");
+            System.out.print("Wybierz opcje: ");
+            option = scanner.nextInt();
+            BookRepositoryImpl bookRepositoryImpl = new BookRepositoryImpl();
+            switch (option) {
+                case 1:
+                    bookRepositoryImpl.delete();
+                    break;
+                case 2:
+                    bookRepositoryImpl.persist();
+                    break;
+                case 3:
+                    bookRepositoryImpl.findById();
+                    break;
+                case 4:
+                    bookRepositoryImpl.findAll();
+                    break;
+                case 0:
+                    System.out.println("Koniec programu");
+                    break;
+                default:
+                    System.out.println("Niepoprawny wybor");
+            }
+        } while (option != 0);
+        HibernateUtil.shutdown();
     }
 }
-
